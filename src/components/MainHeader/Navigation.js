a3542d8f442edf9ff109bba11cf22649c902406d
kavi <sumathi291098@gmail.com>
2023-04-15 13:05:58 +0530

    Rules of Hooks

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AuthContext from '../store/auth-context';
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
+  const ctx = useContext(AuthContext);
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return(
+    
           <nav className={classes.nav}>
       <ul>
         {ctx.isLoggedIn && (
@@ -21,15 +20,12 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
     </nav>
-        )
-      }}
-    
-    </AuthContext.Consumer>
+       
   );
 };
 
